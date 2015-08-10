6e1c8617764fd96e4d1f9632890df1b64fbe1e63
Willis Pinaud <willispinaud@gmail.com>
2015-08-10 15:13:34 -0400

    install bootstrap directives and fix MANIFEST.ini

--- python_virtual/bin/activate.fish
+++ python_virtual/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/willispinaud/Dropbox/Amerique/Montreal/pyrangular/spinalcordtoolbox_web/python_virtual"
+set -gx VIRTUAL_ENV "/Users/willispinaud/Dropbox/Amerique/Montreal/spinalcordtoolbox_web/python_virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
