6e67990baa014843f18e18c35287bd514d594577
Willis Pinaud <willispinaud@gmail.com>
2015-08-11 14:27:49 -0400

    Add button to visualize selected files in brainbrowser. Not working yet, I'm waiting for the directive from SPINE

--- client/app/scripts/controllers/browser.js
+++ client/app/scripts/controllers/browser.js
@@ -8,33 +8,39 @@
  * Controller of the angularSeedApp
  */
 angular.module('angularSeedApp')
-    .controller('BrowserCtrl', ['$scope', '$route', 'SharedDataService',
-    function($scope, $route, SharedDataService) {
-      $scope.NewFile = SharedDataService;
+  .controller('BrowserCtrl', ['$scope', '$route', 'SharedDataService',
+    function ($scope, $route, SharedDataService) {
+      $scope.NewFile = SharedDataService; //it's use to connect upload & browser to detect change and update the tree
 
       $scope.fileViewer = 'Please select a file to view its contents';
 
-        $scope.$watch('NewFile.state', function () {
-        if ($scope.NewFile.state){
+      $scope.$watch('NewFile.state', function () {
+        if ($scope.NewFile.state) { //Update the tree when a file is uploaded
           $route.reload();
         }
-    });
+      });
 
-      $scope.changedCB = function(e, data) {
-      	var nodeChecked = new Array();
-      	var nodePath = [];
-		nodeChecked = angular.element("#jstree").jstree('get_checked');
-		for (var i in nodeChecked){
-			nodePath.push(angular.element("#jstree").jstree("get_node",nodeChecked[i]).original.path);
-		}
-        $scope.$apply(function() {
-            $scope.fileViewer = nodePath;
-          });
+      $scope.changedCB = function (e, data) {
+        var nodeChecked = [];
+        var nodePath = [];
+        nodeChecked = angular.element("#jstree").jstree('get_checked');
+        for (var i in nodeChecked) {
+          nodePath.push(angular.element("#jstree").jstree("get_node", nodeChecked[i]).original.path);
+        }
+        $scope.$apply(function () {
+          $scope.fileViewer = nodePath;
+        });
+      };
+
+      $scope.brainbrowser = function (pathArray) {
+
+        console.log(pathArray);
+
       };
     }
   ]);
 
 
-  //   .each(function(index) {
-		// nodeChecked.push($(this).attr('path'));
-		// });
+//   .each(function(index) {
+// nodeChecked.push($(this).attr('path'));
+// });
