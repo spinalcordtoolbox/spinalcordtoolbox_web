dd9cfedc2cb2ac74d6bfea135cbf232143c1aa16
Willis Pinaud <willispinaud@gmail.com>
2015-08-10 21:51:19 -0400

    Path selection on check is working now!
    But the code is a little bit complicated…

--- client/app/scripts/controllers/toolbox.js
+++ client/app/scripts/controllers/toolbox.js
@@ -12,26 +12,22 @@ angular.module('angularSeedApp')
     function($scope, FetchFileFactory) {
 
       $scope.fileViewer = 'Please select a file to view its contents';
- 		var nodeChecked = [];
 
-      $scope.nodeSelected = function(e, data) {
-      	console.log('selected')
-      	console.log(data)
-      	nodeChecked.push(data.node.original.path);
+      $scope.changedCB = function(e, data) {
+      	var nodeChecked = new Array();
+      	var nodePath = [];
+		nodeChecked = angular.element("#jstree").jstree('get_checked');
+		for (var i in nodeChecked){
+			nodePath.push(angular.element("#jstree").jstree("get_node",nodeChecked[i]).original.path);
+		}
         $scope.$apply(function() {
-            $scope.fileViewer = data;
-          });
-      };
-      $scope.nodeDeselected = function(e, data) {
-      	console.log('nodeDeselected')
-      	console.log(data)
-      	var index = nodeChecked.indexOf(data.node.original.path);
-      	if (index > -1){
-      		nodeChecked.splice(index,1);
-      	}
-        $scope.$apply(function() {
-            $scope.fileViewer = data;
+            $scope.fileViewer = nodePath;
           });
       };
     }
   ]);
+
+
+  //   .each(function(index) {
+		// nodeChecked.push($(this).attr('path'));
+		// });
