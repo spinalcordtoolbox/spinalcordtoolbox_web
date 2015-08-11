21c8bb609ddcd4b051a645a7b6ecc07c98f2db47
Willis Pinaud <willispinaud@gmail.com>
2015-08-11 17:15:38 -0400

    Missing files added to git..

---
+++ client/app/scripts/controllers/arguments.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSeedApp.controller:ArgumentsCtrl
+ * @description
+ * # ArgumentsCtrl
+ * Controller of the angularSeedApp
+ */
+angular.module('angularSeedApp')
+  .controller('ArgumentsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
