8316248892339b68e0f79898789ef93610e81358
Willis Pinaud <willispinaud@gmail.com>
2015-08-11 11:43:29 -0400

    Add Brainbrowser support

---
+++ client/app/scripts/services/shareddataservice.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularSeedApp.SharedDataService
+ * @description
+ * # SharedDataService
+ * Service in the angularSeedApp.
+ */
+angular.module('angularSeedApp')
+  .service('SharedDataService', function () {
+     var NewFile = {
+        path: '',
+        text: '',
+        state: false
+    };
+    return NewFile;
+});
