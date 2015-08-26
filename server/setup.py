01b015f7a7aef1b38914ffbb0c2b4951dedeeb6c
Pierre-Olivier Quirion <pioliqui@gmail.com>
2015-08-25 20:26:07 -0400

    setup install numpy correctly

--- server/setup.py
+++ server/setup.py
@@ -1,6 +1,17 @@
 import os
 
 from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
@@ -59,6 +70,8 @@ setup(name='server',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      cmdclass={'build_ext':build_ext},
+      setup_requires=['numpy'],
       install_requires=requires,
       tests_require=requires,
       test_suite="server",
