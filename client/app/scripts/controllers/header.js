21b5c93e6b5f2299992d6c6a494b492a8ea285f4
Willis Pinaud <willispinaud@gmail.com>
2015-08-30 16:21:14 +0200

    dynamic header part 2: login/logout button

--- client/app/scripts/controllers/header.js
+++ client/app/scripts/controllers/header.js
@@ -8,9 +8,16 @@
  * Controller of the angularSeedApp
  */
 angular.module('angularSeedApp')
-  .controller('HeaderCtrl', function ($scope,$location) {
+  .controller('HeaderCtrl', function ($scope,$location,$localStorage) {
+
+    $scope.$storage = $localStorage;   //Initialization of the local storage
 
     $scope.actif = $location.path();
+
+    $scope.logout = function(){
+      auth.$unauth();
+      $scope.$storage.uid = null;
+    };
 
     $scope.is_active = function(route, view){
       if (route===view){
@@ -21,7 +28,20 @@ angular.module('angularSeedApp')
       }
     };
 
-    $scope.$watch('actif', function () {
+
+
+    $scope.$watch('$storage.uid', function () {
+
+      console.log($scope.$storage.uid);
+
+      if ($scope.$storage.uid===null){
+          $scope.logout_class = "hidden";
+          $scope.login_class = "";
+      }
+      else{
+        $scope.logout_class = "";
+        $scope.login_class = "hidden";
+      }
 
     });
 
